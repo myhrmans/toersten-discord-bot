fa8c92bf9cca775a5ce38ff9043c6536a1103ca6
Martin Myhrman <martin@myhrmans.com>
2019-03-14 22:25:48 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -16,7 +16,7 @@ async def on_ready():
 async def ping(ctx):
     ping_ = bot.latency
     ping = round(ping_ * 1000)
-    await ctx.channel.send(f"It took me {ping}ms to drink a beer and reply to this message, SKÅL... as we say in swedish!")
+    await ctx.channel.send(f"It took me {ping}ms to drink a beer and reply to this message, SKÅL as we say in swedish!")
 
 @bot.command()
 async def report(ctx, member:discord.User = None):
