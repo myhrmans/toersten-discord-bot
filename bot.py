1e833f0f24d77b662f92e29ac08c93121cd5ee68
Martin Myhrman <martin@myhrmans.com>
2019-03-14 13:14:03 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -17,7 +17,7 @@ async def on_ready():
 async def ping(ctx):
     ping_ = bot.latency
     ping = round(ping_ * 1000)
-    await ctx.channel.send(f"It took me {ping}ms to drink a beer and reply to this message, SKÅL..a! as we say in swedish!")
+    await ctx.channel.send(f"It took me {ping}ms to drink a beer and reply to this message, SKÅL...a! as we say in swedish!")
 @bot.command()
 async def version(ctx):
     await ctx.channel.send("Current Version: {}".format(bot_version))
