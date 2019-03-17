523352e0c431bf124723432857ebbf46ea65942e
Martin Myhrman <martin@myhrmans.com>
2019-03-17 21:48:23 +0100

    WIP: Registration

--- bot.py
+++ bot.py
@@ -16,29 +16,51 @@ else:
     bot = commands.Bot(command_prefix="l: ", status=discord.Status.idle, activity=discord.Game(name="Halsar en åbro.."))
 bot.remove_command("help")
 bot_version = "1.00"
-register_list= list()
+register_list = []
 
 class user_register:
-    def __init__(self, member, ID, username, password):
+    def __init__(self, member, ID):
         self.member = member
         self.ID = ID
-        self.username = username
-        self.password = password
-
+        self.username = ""
+        self.password = ""
+    def user_id(self):
+        return self.ID
+    def set_user(self,username):
+        self.username=username
+    def set_password(self,password):
+        self.password=password
+    def get_member(self):
+        return self.member
+    
 class listen_for_request(BaseHTTPRequestHandler):
     def do_POST(self):
         # Doesn't do anything with posted data
         self.send_response(200, "ok")
         content_length = int(self.headers['Content-Length']) # <--- Gets the size of data
-        self.end_headers()
+        #print(self.headers['Content-Length'])
+        self.end_headers() 
         post_data = self.rfile.read(content_length) # <--- Gets the data itself
-        print( "incomming http: ", self.path )
-        print (post_data) # <-- Print post data
+        print( "incomming http: ", self.path ) # <-- Print post data
+        jsona = post_data.decode("utf-8")
+        x = json.loads(jsona)
+        username=x[0]
+        password=x[1]
+        id=x[2]
+        print(username['value'])
+        print(password['value'])
+        print(id['value'])
+        for user in register_list:
+            if(user.user_id()==id['value']):
+                user.set_user(username['value'])
+                user.set_password(password['value'])
+                Thread(target=register,args=(user,)).start()
         #self._set_headers()
 def host_HTTP():
     print("started http")
     httpd = socketserver.TCPServer(("", 3333), listen_for_request)
     httpd.serve_forever()
+    
 @bot.event
 async def on_ready():
     print("Ready to go!")
@@ -47,7 +69,11 @@ async def on_ready():
 
 @bot.command()
 async def ping(ctx):
-    await ctx.channel.send(f"To authenticate open this website: http://158.174.180.57/login?id={secrets.token_urlsafe(32)}")
+    print(type(ctx.message.author))
+    member = ctx.message.author
+    secret = secrets.token_urlsafe(32)
+    register_list.append(user_register(member,secret))
+    await ctx.channel.send(f"To authenticate open this website: http://158.174.180.57/login?id={secret}")
     ping = bot.latency
     ping = round(ping * 1000)
     await ctx.channel.send(f"It took me {ping}ms to drink a beer and reply to this message, SKÅL as we say in swedish!")
@@ -110,37 +136,38 @@ async def courses(ctx, member:discord.User = None):
     for l in courses:
         await member.send(f"{l.text}")
 
-@bot.command()
-async def register(ctx, member:discord.User = None):
-    member = ctx.message.author
-    message = ctx.message
-    def pred(m):
-        return m.author == message.author
-    await member.create_dm()
-    await member.send(f"Ange ditt Blackboard användarnamn (exempel marmyh16):")
-    username = await bot.wait_for('message', check=pred)
-    await member.send(f"Okej {username.content}. Bara ett steg kvar.. ditt lösenord:")
-    password = await bot.wait_for('message', check=pred)
-    #await member.send(f"{username.content} och {password.content}")
+def register(user):
+    print("Register")
+    print(user.__getattribute__("member"))
+    print(user.__getattribute__("ID"))
+    print(user.__getattribute__("username"))
+    print(user.__getattribute__("password"))
+    member = user.get_member()
+    ID = user.__getattribute__("ID")
+    username = user.__getattribute__("username")
+    password = user.__getattribute__("password")
+    print(type(member))
+    await member.__class__.create_dm()
+    await member.__class__.send(f"{username.content} och {password.content}")
     browser = mechanicalsoup.browser = mechanicalsoup.StatefulBrowser(
         soup_config={'features': 'lxml'},
         raise_on_404=True
     )
     login_page = browser.open("https://hh.blackboard.com/webapps/portal/execute/tabs/tabAction?tab_tab_group_id=_98_1")
     login_form = browser.select_form('#loginBoxFull form')
-    browser["user_id"] = username.content
-    browser["password"] = password.content
+    browser["user_id"] = username
+    browser["password"] = password
     resp = browser.submit_selected()
     resp2 = browser.post("https://hh.blackboard.com/webapps/portal/execute/tabs/tabAction", params='action=refreshAjaxModule&modId=_25_1&tabId=_1_1&tab_tab_group_id=_1_1');
     courses = resp2.text
     courses = lxml.html.fromstring(courses)
     courses = courses.cssselect("a")
-    await member.send(f"Om du angivet dina uppgifter rätt kommer här kommer dina kurser:")
+    #await member.send(f"Om du angivet dina uppgifter rätt kommer här kommer dina kurser:")
     for l in courses:
         courseID = l.text.split("HP")
         courseID = courseID[1]
         courseID = courseID[1:7]
-        await member.send(f"{courseID}")
+        #await member.send(f"{courseID}")
 
 @bot.command()
 async def ladok(ctx, member:discord.User = None):
